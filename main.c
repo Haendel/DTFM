36be9cdf774240946a10c1323c005a0e06bc63be
Laurent PABION <pyzealot@gmail.com>
2010-01-20 18:44:09 +0100

    Initial commit

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
+/*
+Author : laurent PABION
+Mail : lpabion <AT> gmail.com
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Normal comment
-/// Documentation comment
+#include "main.h"
+#include "menu.h"
+#include "input_validator.h"
 
-#define MAX_CHARS 300
+
+
+
+
 
 char *asciiLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *numbers = "0123456789";
@@ -13,66 +22,23 @@ char *misc = "_-+*/\\:. ";
 char input[300];
 char test[] = "valid";
 
-/**
-Structures
-*/
 
-struct param
+
+int main(int argc, char *argv[])
 {
-    char workingDirectory[MAX_CHARS];
-    char filesToProcess[MAX_CHARS];
-    char delimiter;
-};
 
+    show_menu_header("Delimited Text File Manager (D.T.F.M.) version 0.0.2", "");
 
-/// Declaration of functions
-int input_is_valid(char *input, char *allowedChars);
-
-
-
-int main(void)
-{
-    printf("Hello world!\n");
     if (input_is_valid(test, asciiLetters))
     {
-        printf("Test OK");
+        printf("Test OK\n");
     }
+
     else
     {
-        printf("Test failed");
+        printf("Test failed\n");
     }
+
+    getchar();
     return 0;
-}
-
-
-
-
-int input_is_valid(char *input, char *allowedChars)
-{
-    unsigned short int charIsAllowed = 0;
-    unsigned short int i = 0;
-
-    while(*input)
-    {
-        charIsAllowed = 0;
-
-        for (i = 0 ; *(allowedChars + i) != '\0' ; i++)
-        {
-            if (*input == *(allowedChars + i))
-            {
-                charIsAllowed = 1;
-                break;
-            }
-        }
-
-        if(charIsAllowed == 1)
-        {
-            input++;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    return 1;
 }
